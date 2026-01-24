ee761f607c832dc76998e685ec3a24df909db12e
Ahmed Wagdy <ahmedwa122@gmail.com>
2026-01-24 19:33:01 +0200

    refactor: simplify app controller and enhance main configuration
    Remove AppService dependency from AppController, returning a static string.
    Delete the associated unit test file as it's no longer needed.
    Update main.ts to add CORS configuration, global validation pipe, set API prefix, and adjust default port.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: [
+      `${process.env.FRONTEND_URL}`, // Allow requests from your frontend URL
+      'http://localhost:3000', // Allow requests from localhost:3000
+    ],
+    methods: ['*'],
+    credentials: true,
+    // origin: true,
+    // allowedHeaders: ['Authorization', 'Content-Type'],
+    allowedHeaders: ['Content-Type'],
+  });
+
+  // validation pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const port = process.env.PORT || 3030;
+
+  // Set the global route prefix
+  app.setGlobalPrefix('api/v1');
+  // await app.listen(process.env.PORT ?? 3030);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
