aeae7d5d34caa9c5dfb9a5b7e5876dad67a635fc
Ahmed Wagdy <ahmedwa122@gmail.com>
2026-01-24 21:28:40 +0200

    feat(users): add user module with CRUD foundation
    Add users module including service, controller, repository, DTOs, and module configuration. Integrates with Prisma for database operations and includes validation decorators for user creation. The module is now imported into the main application module.

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,38 @@
+import {
+  IS_OPTIONAL,
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { Role } from 'generated/prisma/enums';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  role?: Role;
+
+  @IsBoolean()
+  @IsOptional()
+  isActive?: boolean;
+
+  //   doctorProfile?: ;
+}
