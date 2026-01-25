bc48cee6ec19aefb54d997f69ac8203cf17543be
Ahmed Wagdy <ahmedwa122@gmail.com>
2026-01-25 22:25:30 +0200

    feat: Implement Prisma seeding for an admin user, create a login DTO, and configure the seed command.

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import { hash } from 'argon2';
+import { PrismaClient } from '../generated/prisma/client';
+
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+async function main() {
+  // Generate hashed password for admin using argon2
+  const hashedPassword = await hash('123456');
+  // Seed ADMIN
+  const ADMIN = await prisma.user.create({
+    data: {
+      email: 'admin@admin.com',
+      password: hashedPassword,
+      name: 'Admin',
+      role: 'ADMIN',
+      isActive: true,
+    },
+  });
+  console.log('Admin seeded:', ADMIN);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
