d6e525e4eb7f196ba406b79a8653e1fe3cbc0738
Ahmed Wagdy <ahmedwa122@gmail.com>
2026-01-27 12:58:40 +0200

    feat(auth): import JWT and local strategies and UsersModule
    Add JWT and local authentication strategies to the AuthModule providers.
    Import UsersModule to enable user lookup during authentication.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtRefreshStrategy, JwtStrategy, LocalStrategy } from './strategies';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [PassportModule],
+  imports: [PassportModule, UsersModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy, LocalStrategy, JwtRefreshStrategy],
 })
 export class AuthModule {}
