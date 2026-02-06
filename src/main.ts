7156b5fbfa875013e56379dec30ff99c3597aebd
Ahmed Wagdy <ahmedwa122@gmail.com>
2026-02-06 06:04:29 +0200

    feat(security): add Helmet.js middleware for enhanced HTTP security headers
    - Install helmet package as a dependency
    - Apply helmet middleware in main.ts before other middleware
    - Helmet helps secure Express apps by setting various HTTP headers

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,7 +19,7 @@ async function bootstrap() {
     // allowedHeaders: ['Authorization', 'Content-Type'],
     allowedHeaders: ['Content-Type'],
   });
-
+  app.use(helmet());
   // validation pipe
   app.useGlobalPipes(
     new ValidationPipe({
